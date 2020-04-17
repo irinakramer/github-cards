8e9dbe3b4a4cd0207c683461f7207cd4b7966810
Irina Kramer <irakramer@gmail.com>
2020-04-17 16:32:32 -0700

    Updates

---
+++ precache-manifest.dae39e567165349d5aaaafc94b3fb879.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eab741dec9cbc77224a0df7bcb569eff",
+    "url": "/github-cards/index.html"
+  },
+  {
+    "revision": "1b0275862a6d13644667",
+    "url": "/github-cards/static/css/main.67316d0c.chunk.css"
+  },
+  {
+    "revision": "2f5afb238babc71225e4",
+    "url": "/github-cards/static/js/2.530a2fc8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-cards/static/js/2.530a2fc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b0275862a6d13644667",
+    "url": "/github-cards/static/js/main.0f8114e9.chunk.js"
+  },
+  {
+    "revision": "32f4ccedf4327057ce1b",
+    "url": "/github-cards/static/js/runtime-main.9b423e00.js"
+  }
+]);
